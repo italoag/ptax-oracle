f6b80181b391d2e2c001d821232fa5cea85f19b5
Italo A. G <182202+italoag@users.noreply.github.com>
2025-01-20 14:24:26 -0300

    aderyn config

--- test/OraclePtax.t.sol
+++ test/OraclePtax.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import {Test, console} from "forge-std/Test.sol";
 import {OraclePtax} from "../src/OraclePtax.sol";
-import {MockOraclePtax} from "./mocks/MockOraclePtax.sol";
+import {MockOraclePtax} from "./mock/MockOraclePtax.sol";
 
 contract OraclePtaxTest is Test {
     MockOraclePtax public oracle;
